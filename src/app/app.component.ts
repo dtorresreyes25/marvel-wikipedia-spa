ae5b74591ba4026f81ed873e4093317883f09590
Dayron R. <dayronrafael83@icloud.com>
2025-03-19 16:53:57 +0100

    feat: add hero-table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroTableComponent } from './presentation/components/table/table.component';
+import { HeroEntity } from './domain/entities/hero.entity';
+import { HeroMapper } from './domain/mapper/hero.mapper';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: ` <app-hero-table [data]="tableData"></app-hero-table> `,
+  standalone: true,
+  imports: [RouterOutlet, HeroTableComponent]
 })
 export class AppComponent {
-  title = 'marvel-wikipedia-spa';
+  tableData: HeroEntity[] = [];
+
+  constructor() {
+    this.tableData = HeroMapper.fromApi([
+      {
+        nameLabel: 'Ahab',
+        genderLabel: 'male',
+        citizenshipLabel: 'United States of America',
+        skillsLabel: 'superhuman strength',
+        occupationLabel: 'psychologist',
+        memberOfLabel: 'Horsemen of Apocalypse',
+        creatorLabel: 'Walt Simonson'
+      },
+      {
+        nameLabel: 'Anya Corazon',
+        genderLabel: 'female',
+        citizenshipLabel: 'United States of America',
+        skillsLabel: 'superhuman strength',
+        occupationLabel: 'student',
+        memberOfLabel: 'The Spider Society',
+        creatorLabel: 'Fiona Avery'
+      },
+      {
+        nameLabel: 'Banshee',
+        genderLabel: 'male',
+        citizenshipLabel: 'Ireland',
+        skillsLabel: 'sonic scream',
+        occupationLabel: 'criminal',
+        memberOfLabel: 'Interpol',
+        creatorLabel: 'Roy Thomas'
+      }
+    ]);
+  }
 }
---
+++ src/app/presentation/components/table/table.component.ts
@@ -0,0 +1,82 @@
+import {
+  Component,
+  Input,
+  ViewChild,
+  OnChanges,
+  SimpleChanges,
+  AfterViewInit
+} from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { HeroEntity } from '../../../domain/entities/hero.entity';
+
+@Component({
+  selector: 'app-hero-table',
+  template: `
+    <table mat-table [dataSource]="dataSource" matSort>
+      <ng-container matColumnDef="name">
+        <th mat-header-cell *matHeaderCellDef mat-sort-header>Name</th>
+        <td mat-cell *matCellDef="let row">{{ row.name }}</td>
+      </ng-container>
+
+      <ng-container matColumnDef="gender">
+        <th mat-header-cell *matHeaderCellDef mat-sort-header>Gender</th>
+        <td mat-cell *matCellDef="let row">{{ row.gender }}</td>
+      </ng-container>
+
+      <ng-container matColumnDef="citizenship">
+        <th mat-header-cell *matHeaderCellDef mat-sort-header>Citizenship</th>
+        <td mat-cell *matCellDef="let row">{{ row.citizenship }}</td>
+      </ng-container>
+
+      <ng-container matColumnDef="skills">
+        <th mat-header-cell *matHeaderCellDef mat-sort-header>Skills</th>
+        <td mat-cell *matCellDef="let row">{{ row.skills }}</td>
+      </ng-container>
+
+      <ng-container matColumnDef="occupation">
+        <th mat-header-cell *matHeaderCellDef mat-sort-header>Occupation</th>
+        <td mat-cell *matCellDef="let row">{{ row.occupation }}</td>
+      </ng-container>
+
+      <ng-container matColumnDef="memberOf">
+        <th mat-header-cell *matHeaderCellDef mat-sort-header>Member Of</th>
+        <td mat-cell *matCellDef="let row">{{ row.memberOf }}</td>
+      </ng-container>
+
+      <ng-container matColumnDef="creator">
+        <th mat-header-cell *matHeaderCellDef mat-sort-header>Creator</th>
+        <td mat-cell *matCellDef="let row">{{ row.creator }}</td>
+      </ng-container>
+
+      <tr mat-header-row *matHeaderRowDef="columns"></tr>
+      <tr mat-row *matRowDef="let row; columns: columns"></tr>
+    </table>
+  `,
+  styles: [
+    `
+      table {
+        width: 100%;
+      }
+    `
+  ],
+  standalone: true,
+  imports: [MatTableModule, MatSortModule]
+})
+export class HeroTableComponent implements OnChanges, AfterViewInit {
+  @Input() data!: HeroEntity[];
+  @ViewChild(MatSort) sort!: MatSort;
+
+  dataSource = new MatTableDataSource<HeroEntity>();
+  columns = ['name', 'gender', 'citizenship', 'skills', 'occupation', 'memberOf', 'creator'];
+
+  ngOnChanges({ data }: SimpleChanges) {
+    if (data) {
+      this.dataSource.data = data.currentValue || [];
+    }
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+}
