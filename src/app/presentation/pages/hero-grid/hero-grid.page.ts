40ff0ad8f52f4d9286e951716db0cb591f39bd90
Dayron R. <dayronrafael83@icloud.com>
2025-03-20 23:47:32 +0100

    feat(hero-grid): add title

--- src/app/presentation/pages/hero-grid/hero-grid.page.ts
+++ src/app/presentation/pages/hero-grid/hero-grid.page.ts
@@ -10,20 +10,33 @@ import { MatButton } from '@angular/material/button';
 @Component({
   selector: 'app-hero-grid-page',
   template: `
-    <button mat-raised-button color="primary" (click)="addHero()">Create Hero</button>
-    <app-hero-filter (onFilterChange)="searchHeroes($event)"></app-hero-filter>
-    <app-hero-table
-      [data]="heroes$ | async"
-      (onHeroEdited)="editHero($event)"
-      (onHeroSelected)="viewHero($event)"
-      (onHeroDeleted)="deleteHero($event)"
-    ></app-hero-table>
+    <header>
+      <h1>Marvel Heroes</h1>
+      <button mat-raised-button color="primary" (click)="addHero()">Create Hero</button>
+    </header>
+
+    <main>
+      <app-hero-filter (onFilterChange)="searchHeroes($event)"></app-hero-filter>
+      <app-hero-table
+        [data]="heroes$ | async"
+        (onHeroEdited)="editHero($event)"
+        (onHeroSelected)="viewHero($event)"
+        (onHeroDeleted)="deleteHero($event)"
+      ></app-hero-table>
+    </main>
   `,
   styles: [
     `
       button {
         margin: 1rem;
         float: right;
+      }
+
+      header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 1rem;
       }
     `
   ],
